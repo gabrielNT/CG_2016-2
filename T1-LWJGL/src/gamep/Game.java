0bda74839fceb161210a782ed023cd59e11744af
Guilherme Ramos <gmatheus95@outlook.com>
2017-01-05 23:04:48 -0200

    Cleaning the code..

--- T1-LWJGL/src/gamep/Game.java
+++ T1-LWJGL/src/gamep/Game.java
@@ -1,16 +1,9 @@
 package gamep;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import gamep.GamePiece;
 
 import java.nio.*;
 import java.util.*;
-import java.lang.Object;
 import java.awt.Point;
 import org.lwjgl.*;
 import org.lwjgl.glfw.*;
--- T1-LWJGL/src/gamep/GamePiece.java
+++ T1-LWJGL/src/gamep/GamePiece.java
@@ -1,13 +1,6 @@
 package gamep;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.util.*;
-import java.lang.Object;
 import java.lang.Math;
 import java.awt.Point;
 
